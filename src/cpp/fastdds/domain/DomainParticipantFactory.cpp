9d33a4a7e9aef5cb6b267196716e8a2e6ebead50
JLBuenoLopez-eProsima <joseluisbueno@eprosima.com>
2023-10-11 16:38:40 +0200

    Refs #19648: refactor to prevent pointer use
    Signed-off-by: JLBuenoLopez-eProsima <joseluisbueno@eprosima.com>

--- src/cpp/fastdds/domain/DomainParticipantFactory.cpp
+++ src/cpp/fastdds/domain/DomainParticipantFactory.cpp
@@ -57,7 +57,6 @@ DomainParticipantFactory::DomainParticipantFactory()
     , rtps_domain_(fastrtps::rtps::RTPSDomainImpl::get_instance())
     , log_resources_(detail::get_log_resources())
 {
-    type_object_registry_ = std::make_shared<xtypes1_3::TypeObjectRegistry>();
 }
 
 DomainParticipantFactory::~DomainParticipantFactory()
@@ -405,7 +404,7 @@ ReturnCode_t DomainParticipantFactory::set_qos(
     return ReturnCode_t::RETCODE_OK;
 }
 
-std::shared_ptr<xtypes1_3::TypeObjectRegistry> DomainParticipantFactory::type_object_registry()
+xtypes1_3::TypeObjectRegistry& DomainParticipantFactory::type_object_registry()
 {
     return type_object_registry_;
 }
